983bc2cf7cfeacbed354d501c093dd045991e719
khalilboulaalam2003 <khalilcb4324@gmail.com>
2023-10-05 08:21:51 -0700

    bash

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 
-/**
- * main - Entry piont 
- *
- * Description: A C program that prints with put function
- *
- * Return: Alawys 0 (success)
-*/
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle"):
-    return (0);
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
 }
 
