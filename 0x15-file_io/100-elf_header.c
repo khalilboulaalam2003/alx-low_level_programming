bc7ef5e17e3cf486991eacfd9bec803dfee8741e
khalilboulaalam <khalilcb4324@gmail.com>
2023-11-27 09:40:18 -0800

    c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,63 +1,48 @@
 #include "main.h"
 
-/**
- * display_error - Displays an error message to stderr and exits with a status code.
- * @message: The error message to display.
- * @status: The exit status code.
- */
-void display_error(const char *message, int status) {
-    dprintf(2, "%s\n", message);
-    exit(status);
+void print_error_exit(const char *msg) {
+    perror(msg);
+    exit(98);
 }
 
-/**
- * display_elf_header - Displays ELF header information.
- * @header: Pointer to the ELF header.
- */
-void display_elf_header(unsigned char *header) {
+void print_elf_header(const Elf64_Ehdr *header) {
     printf("ELF Header:\n");
-    printf("  Magic:   %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x\n",
-           header[0], header[1], header[2], header[3], header[4], header[5], header[6], header[7],
-           header[8], header[9], header[10], header[11], header[12], header[13], header[14], header[15]);
-    printf("  Class:                             ELF%d\n", header[4] == 1 ? 32 : (header[4] == 2 ? 64 : -1));
-    printf("  Data:                              2's complement, %s endian\n", header[5] == 1 ? "little" : "big");
-    printf("  Version:                           %d (current)\n", header[6]);
-    printf("  OS/ABI:                            %d\n", header[7]);
-    printf("  ABI Version:                       %d\n", header[8]);
-    printf("  Type:                              %d\n", *(unsigned short *)&header[16]);
-    printf("  Entry point address:               0x%x\n", *(unsigned int *)&header[24]);
+    printf("  Magic:   ");
+    for (int i = 0; i < EI_NIDENT; ++i) {
+        printf("%02x%c", header->e_ident[i], i + 1 < EI_NIDENT ? ' ' : '\n');
+    }
+    printf("  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+    printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+    printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+    printf("  OS/ABI:                            %s\n", header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
+    printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+    printf("  Type:                              %s\n", header->e_type == ET_EXEC ? "EXEC (Executable file)" : "Other");
+    printf("  Entry point address:               0x%lx\n", (unsigned long)header->e_entry);
 }
 
-/**
- * main - Entry point for the program.
- * @argc: The number of arguments.
- * @argv: An array of strings containing the arguments.
- *
- * Return: 0 on success, or the exit status code on failure.
- */
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        display_error("Usage: elf_header elf_filename", 98);
+        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+        exit(98);
     }
 
     int fd = open(argv[1], O_RDONLY);
     if (fd == -1) {
-        display_error("Error opening file", 98);
+        print_error_exit("open");
     }
 
-    unsigned char elf_header[ELF_HEADER_SIZE];
-    ssize_t bytes_read = read(fd, elf_header, ELF_HEADER_SIZE);
-    if (bytes_read == -1 || bytes_read != ELF_HEADER_SIZE) {
-        close(fd);
-        display_error("Error reading ELF header", 98);
+    Elf64_Ehdr elf_header;
+    if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr)) {
+        print_error_exit("read");
     }
 
-    display_elf_header(elf_header);
-
-    if (close(fd) == -1) {
-        display_error("Error closing file descriptor", 98);
+    if (lseek(fd, 0, SEEK_SET) == -1) {
+        print_error_exit("lseek");
     }
 
+    print_elf_header(&elf_header);
+
+    close(fd);
     return 0;
 }
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,16 +5,25 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
 
-#define BUFFER_SIZE 1024
-#define ELF_HEADER_SIZE 64
+/* Function declarations */
 
+/* 0-read_textfile.c */
 ssize_t read_textfile(const char *filename, size_t letters);
+
+/* 1-create_file.c */
 int create_file(const char *filename, char *text_content);
+
+/* 2-append_text_to_file.c */
 int append_text_to_file(const char *filename, char *text_content);
-void display_error(const char *message, int status);
-void display_elf_header(unsigned char *header);
+
+/* 3-cp.c */
+void print_error_exit(const char *msg);
+void copy_file(const char *src_filename, const char *dest_filename);
+
+/* 100-elf_header.c */
+#include <elf.h>
+void print_elf_header(const Elf64_Ehdr *header);
 
 #endif /* MAIN_H */
 
