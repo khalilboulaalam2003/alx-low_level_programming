7f36a235151c9fcac5f58716cabd9eca76a7742f
khalilboulaalam <khalilcb4324@gmail.com>
2023-11-28 00:43:52 -0800

    c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,57 +2,58 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include "main.h"
 
 /**
- * read_textfile - Read from a file and output to standard output.
+ * read_textfile - Reads from a file and outputs to standard output.
  * @filename: The file name.
- * @letters: Number of bytes the function is allowed to write to STDOUT.
- *
+ * @letters: The number of bytes the function is allowed to write to STDOUT.
  * Return: The count of all characters in the file.
  */
 ssize_t read_textfile(const char *filename, ssize_t letters)
 {
-    int fp;
+    int file_descriptor;
     ssize_t write_count, read_count;
-    char *BUFFER;
+    char *buffer;
 
-    if (filename == NULL || letters == 0) /* File doesn't exist or invalid letters count */
+    if (filename == NULL || letters == 0) /* File doesn't exist or invalid letters */
         return (0);
 
     /* Open the file */
-    fp = open(filename, O_RDONLY);
-    if (fp == -1)
+    file_descriptor = open(filename, O_RDONLY);
+    if (file_descriptor == -1)
     {
         return (0);
     }
 
-    /* Handle allocation */
-    BUFFER = malloc(sizeof(char) * letters);
-    if (BUFFER == NULL)
+    /* Allocate memory for the buffer */
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
     {
-        free(BUFFER);
+        close(file_descriptor);
         return (0);
     }
 
     /* Read bytes of size letters from the file */
-    read_count = read(fp, BUFFER, letters);
+    read_count = read(file_descriptor, buffer, letters);
     if (read_count == -1)
     {
-        free(BUFFER);
+        free(buffer);
+        close(file_descriptor);
         return (0);
     }
 
     /* Write the bytes of size letters to STDOUT */
-    write_count = write(1, BUFFER, read_count);
+    write_count = write(1, buffer, read_count);
     if (write_count == -1 || (write_count != read_count))
     {
-        free(BUFFER);
+        free(buffer);
+        close(file_descriptor);
         return (0);
     }
 
-    free(BUFFER);
-    close(fp);
+    /* Clean up and close the file */
+    free(buffer);
+    close(file_descriptor);
+
     return (write_count);
 }
-
